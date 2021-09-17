bd72aa5f6aa084dbbf164041670d060e8f27897e
Harris Das <harrisdas0@gmail.com>
2021-09-18 02:50:53 +0500

    - add di

--- app/src/main/java/com/harris/mygroceryshop/ui/MainActivity.kt
+++ app/src/main/java/com/harris/mygroceryshop/ui/MainActivity.kt
@@ -1,16 +1,20 @@
-package com.harris.mygroceryshop
+package com.harris.mygroceryshop.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.harris.mygroceryshop.ui.theme.MyGroceryShopTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    val viewModel: ProductViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
---
+++ app/src/main/java/com/harris/mygroceryshop/ui/ProductViewModel.kt
@@ -0,0 +1,9 @@
+package com.harris.mygroceryshop.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProductViewModel @Inject constructor() : ViewModel() {
+}
