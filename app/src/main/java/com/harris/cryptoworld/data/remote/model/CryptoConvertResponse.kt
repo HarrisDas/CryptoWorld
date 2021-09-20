f4089ed987b692b7eebaa7ef73f7dc09cdd9e758
Harris Das <harrisdas0@gmail.com>
2021-09-21 01:38:52 +0500

    - use case updated

--- app/src/main/java/com/harris/cryptoworld/data/remote/model/CryptoConvertResponse.kt
+++ app/src/main/java/com/harris/cryptoworld/data/remote/model/CryptoConvertResponse.kt
@@ -1,13 +1,17 @@
 package com.harris.cryptoworld.data.remote.model
 
 import com.google.gson.annotations.SerializedName
+import com.harris.cryptoworld.domain.model.CryptoConvert
 
 class CryptoConvertResponse : ApiResponse() {
+
     @SerializedName("info")
     val info: CryptoConvertInfo? = null
 
     @SerializedName("result")
-    val result: Double? = null
+    val result: Double = 0.0
+
+    fun toBean() = CryptoConvert(result, info?.rate ?: 0.0)
 }
 
 class CryptoConvertInfo {
--- app/src/main/java/com/harris/cryptoworld/domain/interactors/convertcrypto/ConvertCryptoUseCaseImpl.kt
+++ app/src/main/java/com/harris/cryptoworld/domain/interactors/convertcrypto/ConvertCryptoUseCaseImpl.kt
@@ -1,10 +1,17 @@
 package com.harris.cryptoworld.domain.interactors.convertcrypto
 
+import android.accounts.NetworkErrorException
+import com.harris.cryptoworld.domain.model.CryptoConvert
 import com.harris.cryptoworld.domain.repository.ICryptoRepository
 import javax.inject.Inject
 
 class ConvertCryptoUseCaseImpl @Inject constructor(private val repository: ICryptoRepository) :
     ConvertCryptoUseCase {
-    override fun invoke(from: String, to: String, amount: Double) {
+    override suspend fun invoke(from: String, to: String, amount: Double): CryptoConvert {
+        val convertCrypto = repository.convertCrypto(from, to, amount)
+        if (convertCrypto.success == true) {
+            return convertCrypto.toBean()
+        }
+        throw NetworkErrorException("Unable to convert!")
     }
 }
